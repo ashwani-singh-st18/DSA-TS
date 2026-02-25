f269a60f9e3c20128004512dff0363d5a9743a17
ashwani-singh-st18 <ashwani-singh-st18@staytuned.website>
2026-02-25 10:21:20 +0530

    Starts With method

--- DataStructures/Trie/Trie.ts
+++ DataStructures/Trie/Trie.ts
@@ -38,5 +38,16 @@ class Trie{
         }
         return current.isEndOfWord;
     }
+    startsWith(prefix: string): boolean{
+        let current = this.root;
+
+        for(let char of prefix){
+            if(!current.children[char]){
+                return false;
+            }
+            current = current.children[char];
+        }
+        return true;
+    }
 }
 
